7ed8cdbe6941772a2a4c2250b37ab12b3b5375b3
prayag-growexx <prayag.talaviya@growexx.com>
2023-09-07 19:10:20 +0530

    Logical operators

--- operators/logicalOperators.php
+++ operators/logicalOperators.php
@@ -1,3 +1,49 @@
 <?php
+    $x = 100;  
+    $y = 50;
 
-?>
+    // ?	True if both $x and $y are true
+    if ($x == 10 and $y == 50) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+    echo "<br>";
+
+    if ($x == 100 && $y == 50) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+    echo "<br>";
+
+    // ?	True if either $x or $y is true
+    if ($x == 10 or $y == 50) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+    echo "<br>";
+
+    if ($x == 10 || $y == 0) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+    echo "<br>";
+    
+    // ?	True if either $x or $y is true, but not both
+    if ($x == 100 Xor $y == 50) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+    echo "<br>";
+
+    // ? True if $x is not true
+    if (!($x == 10)) {
+        echo "True";
+    } else {
+        echo "False";
+    }
+?>  
