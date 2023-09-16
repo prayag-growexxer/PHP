58faab23b697b9d6c5856421a01334225bc82d49
prayag-growexx <prayag.talaviya@growexx.com>
2023-09-16 20:37:59 +0530

    Advance topic include and require

---
+++ includeAndRequire/index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>include and require</title>
+</head>
+<body>
+    <!-- require will produce a fatal error (E_COMPILE_ERROR) and stop the script -->
+    <!-- include will only produce a warning (E_WARNING) and the script will continue -->
+    <?php 
+        // include 'prayag.php'; 
+        // echo "it is working";
+    ?>
+    
+    <?php 
+        // require 'prayag.php'; 
+        // echo "it is working";
+    ?>
+
+    <?php
+        include 'var.php';
+        echo $myProjectName."<br>";
+        echo $startDate;
+    ?>
+    
+    <?php include "copy.php" ?>
+
+
+</body>
+</html>
---
+++ includeAndRequire/var.php
@@ -0,0 +1,4 @@
+<?php
+    $myProjectName = "PHP learning";
+    $startDate = date("d/m/y",mktime(12,59,59,6,1,2023))
+?>
